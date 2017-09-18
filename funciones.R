d8994dab8ac94386b1ca30dd29daf46ba341ef08
Mauricio <MauricioTrejo@users.noreply.github.com>
2017-09-18 15:12:19 -0500

    CIGUB function
    Still working on this function

--- funciones.R
+++ funciones.R
@@ -1,12 +1,18 @@
+#Returns the instruments that a fund can buy.
 instrumentocompra <- function(precios,nombre){
+  cigub <- c("B","BI","E","F","IP","IS","IT","L","L0","LS","LT","M","M0","M3","M5","M7","MC","MP","N","S","S0","S3","S5","T","T3","T5")
   valores <- switch(nombre,
-  "+CIGUB"={unique(precios$Instrumento[c(which(precios$Mercado == "EC"),which(precios$Mercado == "EO"),
-             which(precios$Mercado == "ER"),which(precios$Mercado == "MC"),which(precios$Mercado == "NR"))])}
+  "+CIGUB"={inst(precios,cigub)}
   )
   return(valores)
 }
-
+#Returns the instruments that a fund can sell.
 instrumentoventa <- function(fondos,nombre){
   valores <- unique(fondos$Emisora[which(fondos$Fondo == nombre)])
   return(valores)
 }
+#Function to supply elements in the market of a fund.
+inst <- function(precios, include){
+  precios$Instrumento[ a %in% include]
+}
+
