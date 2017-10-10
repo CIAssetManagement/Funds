0d7ea2bcae2fab3ac1b96feabb956464a61b74d3
Mauricio <MauricioTrejo@users.noreply.github.com>
2017-10-10 11:30:51 -0500

    Changes

--- server.R
+++ server.R
@@ -2,15 +2,9 @@ library(shiny)
 library(ggplot2)
 library(DT)
 library(dplyr)
-library(readxl)
-library(xts)
-library(quantmod)
 
 #Bases de datos
-precios <- read.csv("Precios.csv", header=TRUE)
-precios2 <- read_xlsx("Precios2.xlsx")
-Fecha <- precios2$Fecha
-precios2$Fecha <- NULL
+precios <- read.csv("Precios.csv",header = TRUE)
 precios <- cbind(precios,Tipo = sub("-.*","",precios$Instrumento))
 fondos <- read.csv("Fondos.csv",header = TRUE)
 #Tener cuidado si existe un tipo de valor z
@@ -39,8 +33,7 @@ instrumentocompra <- function(precios,nombre, mercados){
                     "+CIGUMP"={precios$Instrumento[deudagub]},
                     "+CIGULP"={precios$Instrumento[deudagub]},
                     "+CIPLUS"={precios$Instrumento[c(deudagub,deudacorp,pagares)]},
-                    #"+CIBOLS"={precios[c(deudagub,stocksmx,stocksint,fondos,trac)]},
-                    "+CIBOLS"={colnames(precios2)},
+                    "+CIBOLS"={precios$Instrumento[c(deudagub,stocksmx,stocksint,fondos,trac)]},
                     "+CIUSD"={precios$Instrumento[c(usd,trac)]},
                     "+CIEQUS"={precios$Instrumento[c(stocksint,trac,usd)]}
   )
@@ -61,6 +54,7 @@ function(input, output, session) {
   
   #Calculo del monto o titulos venta
   montovv <- function(monto, precio){
+    
     if(monto == 0){
       monto = input$titulosv*precio
     }
@@ -123,82 +117,17 @@ function(input, output, session) {
     return(rowdatav)
   })
   
-  ####################################### Indicadores ##############################################
-  
-  portafolio <- fondos %>% filter(Fondo == "+CIBOLS" & I == "D")
-  indices <- array(match(portafolio$Emisora, colnames(precios2)))
-  titulos <- portafolio$Títulos / sum(portafolio$Títulos)
-  
-  
-  portafolioa <- t(titulos * t(precios2[indices]))
-  prices <- xts(portafolioa,as.Date(Fecha))
-  rend <- rowSums(ROC(prices, type = "discrete"),na.rm = TRUE)
-  
-  ###Calculo del var###
-
-  varant <- quantile(rend,0.05)
-  vardes <- 0.05   
-  
-  ###Calculo del rendimineto###
-  rendacu1 <- (prod(1 + rend/100) - 1)
-  
-  #Mensaje de error para la venta
-  if(varant<vardes){
-  observeEvent(input$addv, {
-    showModal(modalDialog(title = "ERROR",paste0("No se puede vender ",
-    titulosvv(input$titulosv,preciovv())," titulos de ",input$instrumentov," sobrepsa el VaR permitido")))
-  })}
-  
-  #Mensaje de error para la compra
-  # if(varant<vardes){
-  # observeEvent(input$addc, {
-  #   showModal(modalDialog(title = "ERROR",paste0("No se puede comprar ",
-  #   tituloscc(input$titulosg,preciocc())," titulos de ",input$instrumentoc," sobrepsa el VaR permitido")))
-  # })}
-  # 
-  # #Data frame para alimentar la tabla con los instrumentos compra
-  # dfc <- eventReactive(input$addc,{
-  #   fondc <- input$fondo
-  #   validate(need(input$instrumentoc != "", "Favor de seleccionar un instrumento"))
-  #   instrumentocc <- input$instrumentoc
-  #   montoc <- montocc(input$montog,preciocc())
-  #   titulosc <- tituloscc(input$titulosg,preciocc())
-  #   
-  #   new_rowc <- data.frame(Fondo=fondc,Instrumento=instrumentocc,Monto=montoc,Titulos= titulosc)
-  #   rowdatac <<- rbind(rowdatac,new_rowc)
-  #   return(rowdatac)
-  # })
-  
-  #Data frame foto actual Fondos
-  dfinda <-data.frame( Fondo= "CIBOLS",VaR=varant,Rendimiento=rendacu1)
-  
-  #Data frame simulador
-  rowdata <- c()
-  dfindd <- eventReactive(input$addv | input$addc,{
-    fond <- input$fondo
-    VaR <- varant
-    rendd <- rendacu1
-
-    newrow <- data.frame(Fondo=fond,VaR=varant,Rendimiento=rendd)
-    rowdata <<- rbind(rowdata,newrow)
-    return(rowdata)
+  #Data frame para alimentar la tabla con los instrumentos compra
+  dfc <- eventReactive(input$addc,{
+    fondc <- input$fondo
+    instrumentocc <- input$instrumentoc
+    montoc <- montocc(input$montog,preciocc())
+    titulosc <- tituloscc(input$titulosg,preciocc())
+    
+    new_rowc <- data.frame(Fondo=fondc,Instrumento=instrumentocc,Monto=montoc,Titulos=titulosc)
+    rowdatac <<- rbind(rowdatac,new_rowc)
+    return(rowdatac)
   })
-  
-  
-  #Bonton delete row
-  # del <- eventReactive(input$deleteSelected,{
-  #   if(!is.null(input$indd_rows_selected)){
-  #     rowdata <- rowdata[-as.numeric(input$indd_rows_selected),]
-  #   }
-  #   return(rowdata)
-  # })
-  
-  output$ventav <- renderTable({dfv()})
-  output$comprac <- renderTable({dfc()})
-  
-  options(DT.options = list(pageLength = 5))
-  output$inda = DT::renderDataTable({dfinda})
-  output$indax=renderPrint(input$inda_rows_selected)
-  output$indd = DT::renderDataTable({dfindd()})
-  output$inddx=renderPrint(input$indd_rows_selected)
+  output$ventav = DT::renderDataTable({dfv()})
+  output$comprac = DT::renderDataTable({dfc()})
 }
